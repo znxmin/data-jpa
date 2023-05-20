29a48d817930ab5a9a04e447b27b9167ad71b23a
znxmin <znxmin@gmail.com>
2023-05-20 13:28:48 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,15 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,8 +32,29 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members-old")
+    public Page<Member> list(@PageableDefault(size = 5, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    // 페이징 정보가 둘 이상이면 접두사로 구분 (@Qualifier에 접두사명 추가)
+    // /members?member_page=0&order_page=1
+    // @GetMapping("/members")
+    // public String list(@Qualifier("member") Pageable memberPageable,
+    //                    @Qualifier("order") Pageable orderPageable) {
+    //     return ...;
+    // }
+
+    @GetMapping("/members")
+    public Page<MemberDto> listOfPageDto(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
